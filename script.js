2accccc0847b7bd4db653b6dc6be2b6b05afa882
Zhuoxinran Li <zxrli.zoe@gmail.com>
2025-01-28 08:14:05 -0500

    Jan,29th edit
    add a chat box, but need to be further refined.
    finish the refinement before submitting

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// Create variable for button element using ID
-const button = document.getElementById("my-btn");
+// document.in JavaScript is calling an existing element from the HTML.
 
-// Define function to return message
-const alertMessage = () => {
-    alert("You clicked me!");
-};
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Notes from the Radiu3 loaded!");
 
-// Add event listener to button element with function to open dialogue box with message
-button.addEventListener("click", alertMessage);
+    // Venue cards: when move the mouse over, the card will slightly
+    const venueCards = document.querySelectorAll(".venue-card");
+    venueCards.forEach(card => {
+        card.addEventListener("mouseover", function () {
+            this.style.transform = "scale(1.05)";
+            this.style.transition = "transform 0.3s ease-in-out";
+        });
+        card.addEventListener("mouseout", function () {
+            this.classList.remove("shadow-lg");
+            this.style.transform = "scale(1)";
+        });
+    });
+
+});
